6921d1616e8936f82294d18d0461616707aed11f
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2022-02-19 19:53:43 +0700

    feat: add index.html, css, js

---
+++ script.js
@@ -0,0 +1,69 @@
+let gameSection = document.querySelector('.game__section');
+let win = document.querySelector('.resalt__text-win');
+let clear = document.querySelector('.button__newgame');
+let boxes = document.querySelectorAll('.game__section-box');
+let resaltSection = document.querySelector('.resalt__section');
+let resaltCount = document.querySelector('.resalt__text-count');
+let audioClick = document.querySelector('.click');
+let audioTada = document.querySelector('.tada');
+let count = 0;
+let result = '';
+let playerOne;
+let playerTwo;
+
+gameSection.addEventListener('click', function (elem) {
+    audioClick.play();
+    if (elem.target.className == "game__section-box") {
+        if (count % 2 === 0) {
+            elem.target.textContent = 'x';
+
+        } else {
+            elem.target.textContent = 'o';
+
+        }
+
+        count++;
+        check();
+    }
+    resaltCount.textContent = count;
+});
+
+function check() {
+    let arr = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (boxes[arr[i][0]].textContent == 'x' && boxes[arr[i][1]].textContent == 'x' && boxes[arr[i][2]].textContent == 'x') {
+            playerOne = true;
+            result = 'крестики';
+            win.textContent = result;
+            resaltSection.style.display = 'block';
+            audioTada.play();
+        } else if (boxes[arr[i][0]].textContent == 'o' && boxes[arr[i][1]].textContent == 'o' && boxes[arr[i][2]].textContent == 'o') {
+            playerTwo = true;
+            result = 'нолики';
+            win.textContent = result;
+            resaltSection.style.display = 'block';
+            audioTada.play();
+        }
+    }
+}
+
+clear.addEventListener('click', function () {
+    location.reload();
+    boxes.forEach(function (box) {
+
+        box.textContent = "";
+        win.textContent = "";
+        resaltSection.style.display = 'none';
+    });
+});
